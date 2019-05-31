d2ac7e9e800c47080bc2782ead765fb3a6e0d449
Colourz5 <donnyyamsiri1@gmail.com>
2019-05-31 17:15:33 +1000

    HARD... until you use .find()

--- week8/rna.py
+++ week8/rna.py
@@ -1,31 +1,17 @@
-import time
-
-
 def spliceosome(rna):
     intron_open = "GUGU"
     intron_close = "AGAG"
-    count = 0
-    starttime = time.perf_counter()
-    # while intron_open in rna and intron_close in rna:
-    while count < 10:
-        temp = rna
-        start = rna.index(intron_open)
-        end = rna.index(intron_close) + 4
-        rna = rna[:start] + rna[end:]
-        count += 1
-        print("Iteration: {}".format(count))
-        print(rna)
-        if rna == temp:
+    while True:
+        start = rna.find(intron_open)
+        if start == -1:
             break
-    timetaken = time.perf_counter() - starttime
-    print("This took {} seconds".format(timetaken))
+        end = rna.find(intron_close, start + 4) + 4
+        if end == -1:
+            break
+        rna = rna[:start] + rna[end:]
     return rna
 
 
 rna = input("Input strand: ")
 rna = spliceosome(rna)
 print("\nOutput is {}".format(rna))
-
-# Program runs too slow
-# GUGUAGAGGUCACAGUGUAAAAGCUCUAGAGCAGACAGAUGUAGAGGUGUUGUGUAACCCGUAGAGCAAAGGCAACAGUGUGUAAAGAGGUGUAAAGAG
-# GUCACACAGACAGAUGUAGAGGUGUUGUGUAACCCGUAGAGCAAAGGCAACAGUGUGUAAAGAGGUGUAAAGAG
